b6e91e61a2b9deb8017fc558adfbfadd0fc8bb72
Tevf0 <tevfikemresungur@gmail.com>
2022-09-23 10:28:52 +0300

    dm addition

--- events/interactionEvents/interactionCreate.js
+++ events/interactionEvents/interactionCreate.js
@@ -1,5 +1,6 @@
 const discord = require("discord.js");
 const {MongoClient} = require("mongodb")
+const {EmbedBuilder , ActionRowBuilder , ButtonBuilder , ButtonStyle} = discord;
 
 
 // Client interactionCreate event
@@ -18,19 +19,35 @@ module.exports = {
 
         client.on("interactionCreate", async (interaction) => {
 
-            if(interaction.isCommand()){
-                addXP(mongoClient, interaction.user.id, 0.1)
+            if(interaction.isButton()){
+                if(interaction.customId === "teamApply"){
 
-                if(interaction.commandName === 'rps'){
-                    rpsGame(mongoClient, interaction);
-                }
-                if(interaction.commandName === 'showxp'){
-                    showXP(mongoClient, interaction);
-                }
-                if(interaction.commandName === "leaderboard"){
-                    leaderboard(mongoClient,interaction);
-                }
+                    const embed = new EmbedBuilder()
+                        .setTitle("Joining Request To Your Team")
+                        .setDescription("${username} from the Algo Teams wants to join to your team! If you're interested, you can interact with ${username} and accept the request.")
+                        .setThumbnail("https://media.istockphoto.com/vectors/agreement-color-line-icon-documentation-status-linear-vector-request-vector-id1271490971?k=20&m=1271490971&s=612x612&w=0&h=AuGYSNj2B9lBBFWZ4CWaI39-VXxYE_b4EMzsbLR8OC4=")
+                        .setColor("Random");
 
+                    const row = new ActionRowBuilder()
+                        .addComponents(new ButtonBuilder()
+                            .setCustomId("acceptButton")
+                            .setLabel("Accept")
+                            .setEmoji("✅")
+                            .setStyle(ButtonStyle.Primary)
+                        )
+                        .addComponents(new ButtonBuilder()
+                            .setCustomId("rejectButton")
+                            .setLabel("Reject")
+                            .setEmoji("❌")
+                            .setStyle(ButtonStyle.Secondary)
+                        );
+
+                    interaction.user.send({
+                        embeds: [embed],
+                        components: [row]
+                    })
+
+                }
             }
 
         });
